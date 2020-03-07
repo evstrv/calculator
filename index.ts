885ea49462b455ac3e24b463498049bc0b1c7266
evstrv <nice.evstratov@ya.ru>
2020-03-07 13:17:48 +0300

    calc

--- index.ts
+++ index.ts
@@ -9,5 +9,19 @@ class Calculate {
 
     private render = () => {
         this.input = document.createElement('input');
+        const controls: Array<Array<string>> = [
+            ['7', '8', '9', '*'],
+            ['4', '5', '6', '/'],
+            ['1', '2', '3', '-'],
+            ['', '0', '=', '+']
+        ];
+
+        for(const ar of controls) {
+            for(const i of ar) {
+                if() {
+                    const el: HTMLDivElement = document.createElement('div');
+                }
+            }
+        }
     };
 }
